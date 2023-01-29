011a75c110801e1a43364cf4022ddf0297e219d5
Gerorge <logovas4@gmail.com>
2023-01-29 18:11:32 +0300

    Task1 added

---
+++ task1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] RandomArrayCreation(int size)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(100, 1000);
+    }
+    return array;
+}
+
+int CounterOfParity(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)    
+    {
+      if (arr[i] % 2 == 0)
+      {
+        counter++;
+      }
+    }
+    return counter;
+}
+
+int size = 4;
+int[] arr = RandomArrayCreation(size);
+int numb = CounterOfParity(arr);
+Console.WriteLine("[" + string.Join(",", arr) + "]");
+Console.WriteLine($"The number of even numbers is {numb}.");
